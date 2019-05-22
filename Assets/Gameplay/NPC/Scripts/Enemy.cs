d691abbbf403e13e4969e10ba67d88dc6aa15d81
threepat1 <42788704+threepat1@users.noreply.github.com>
2019-05-22 16:07:40 +1000

    edit enemy

--- Assets/Gameplay/NPC/Scripts/Enemy.cs
+++ Assets/Gameplay/NPC/Scripts/Enemy.cs
@@ -11,10 +11,16 @@ namespace SuperShooter
 
         public int health = 100;
         public int damage = 25;
+        float timer;
+        public float timeBetweenAttacks = 1f;
 
-        public Transform player;
+        public Transform playPos;
         private NavMeshAgent nav;
 
+        public GameObject player;
+
+        bool playerInRange;
+ 
         // ------------------------------------------------- //
 
         public void DealDamage(int amount)
@@ -29,20 +35,63 @@ namespace SuperShooter
 
 
         // ------------------------------------------------- //
-        public void Start()
+        public void Awake()
         {
+            player = GameObject.FindGameObjectWithTag("Player");
             nav = GetComponent<NavMeshAgent>();
         }
-        public void Update()
-        {
-            nav.SetDestination(player.position);
-        }
-        public void OnCollisionEnter(Collision col)
+
+        private void OnTriggerEnter(Collider col)
         {
             if (col.gameObject.tag == ("Player"))
             {
-                DealDamage(damage);
+                
                 print("yes!!");
+                playerInRange = true;
+            }
+        }
+        void OnTriggerExit(Collider other)
+        {
+            // If the exiting collider is the player...
+            if (other.gameObject.tag == "Player")
+            {
+                // ... the player is no longer in range.
+                playerInRange = false;
+            }
+
+        }
+        void Update()
+        {
+            nav.SetDestination(playPos.position);
+            // Add the time since Update was last called to the timer.
+            timer += Time.deltaTime;
+
+            // If the timer exceeds the time between attacks, the player is in range and this enemy is alive...
+            if (timer >= timeBetweenAttacks && playerInRange) // && enemyHealth.currentHealth > 0
+            {
+                // ... attack.
+                Attack();
+            }
+
+            // If the player has zero or less health...
+            if (health <= 0)
+            {
+                // ... tell the animator the player is dead.
+                //  anim.SetTrigger("PlayerDead");
+            }
+        }
+
+
+        void Attack()
+        {
+            // Reset the timer.
+            timer = 0f;
+
+            // If the player has health to lose...
+            if (health > 0)
+            {
+                // ... damage the player.
+                DealDamage(damage);
             }
         }
     }
